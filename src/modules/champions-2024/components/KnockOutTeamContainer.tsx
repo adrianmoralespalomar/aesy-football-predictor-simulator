0efdb8d5b0f95ae5b234affbef0643e389304c4a
Morales Palomar <adrian.morales@cunef.edu>
2024-06-17 14:02:46 +0200

    minimal changes

--- src/modules/champions-2024/components/KnockOutTeamContainer.tsx
+++ src/modules/champions-2024/components/KnockOutTeamContainer.tsx
@@ -25,13 +25,13 @@ export function KnockOutTeamContainer(props:KnockOutTeamContainerProps){
     <>
     <div className = "[&>button]:rounded-full [&>button.selected-team]:bg-green-500 [&>button:not(.selected-team):first-child]:bg-purple-500 [&>button:not(.selected-team):last-child]:bg-blue-800  flex flex-row items-center">
       <button disabled = {!localTeam} className = {`hover:brightness-75 ease-in duration-300 ${selectedTeamWon === props.localRivalIndex ? 'selected-team' : ''}`}  onClick={() => selectTeamWon(props.localRivalIndex)}>
-        <img className = "w-12 h-12 p-1" src={`/public/assets/teams-logo/${localTeam?.abbreviation}.webp`} alt="" onError={(e) => e.currentTarget.src = '/public/assets/teams-logo/notfoundpicture.webp'} />
+        <img className = "w-12 h-12 p-1" src={`${TEAMS_LOGOS_FOLDER}${localTeam?.abbreviation}.webp`} alt="" onError={(e) => e.currentTarget.src = `${TEAMS_LOGOS_FOLDER}notfoundpicture.webp`} />
       </button>
 
       <div className = "w-1 h-0.5 bg-slate-50"></div>
       
       <button disabled = {!awayTeam} className = {`hover:brightness-75 ease-in duration-300 ${selectedTeamWon === props.awayRivalIndex ? 'selected-team' : ''}`} onClick={() => selectTeamWon(props.awayRivalIndex)}>
-        <img className = "w-12 h-12 p-1" alt="" src={`${TEAMS_LOGOS_FOLDER}${awayTeam?.abbreviation}.webp`} onError={(e) => e.currentTarget.src = `${TEAMS_LOGOS_FOLDER}/notfoundpicture.webp`}  />
+        <img className = "w-12 h-12 p-1" alt="" src={`${TEAMS_LOGOS_FOLDER}${awayTeam?.abbreviation}.webp`} onError={(e) => e.currentTarget.src = `${TEAMS_LOGOS_FOLDER}notfoundpicture.webp`}  />
       </button>
     </div>
       
